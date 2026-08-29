93e3a0bc56196ce6712d7c2a1a202350ad4f7484
Nadezhda  Samartseva <nadya.sm101@yandex.com>
2026-08-29 19:20:26 +0300

    compiler: Add test for CL_PROGRAM_SOURCE with clCreateProgramWithBinary

--- test_conformance/compiler/test_build_helpers.cpp
+++ test_conformance/compiler/test_build_helpers.cpp
@@ -1120,3 +1120,79 @@ REGISTER_TEST(kernel_name_size)
     }
     return TEST_PASS;
 }
+
+REGISTER_TEST(get_program_source_from_binary)
+{
+    cl_int error = CL_SUCCESS;
+    clProgramWrapper program;
+    clProgramWrapper program_from_binary;
+
+    program = clCreateProgramWithSource(
+        context, 1, sample_kernel_code_single_line, nullptr, &error);
+    test_error(error, "clCreateProgramWithSource failed");
+
+    error = clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr);
+    test_error(error, "clBuildProgram failed");
+
+    size_t binary_size = 0;
+    error = clGetProgramInfo(program, CL_PROGRAM_BINARY_SIZES,
+                             sizeof(binary_size), &binary_size, nullptr);
+    test_error(error, "clGetProgramInfo for binary size failed");
+
+    if (binary_size == 0)
+    {
+        log_error("Binary size is zero\n");
+        return TEST_FAIL;
+    }
+
+    std::vector<unsigned char> binary(binary_size);
+    unsigned char* binary_ptr = binary.data();
+
+    error = clGetProgramInfo(program, CL_PROGRAM_BINARIES,
+                             sizeof(binary_ptr), &binary_ptr, nullptr);
+    test_error(error, "clGetProgramInfo for binary failed");
+
+    const unsigned char* binary_data = binary.data();
+    cl_int binary_status = CL_SUCCESS;
+
+    program_from_binary = clCreateProgramWithBinary(
+        context, 1, &device, &binary_size, &binary_data, &binary_status,
+        &error);
+    test_error(error, "clCreateProgramWithBinary failed");
+    test_assert_error(binary_status == CL_SUCCESS,
+                      "Binary status is not CL_SUCCESS");
+
+    size_t src_size = 0;
+    error = clGetProgramInfo(program_from_binary, CL_PROGRAM_SOURCE,
+                             0, nullptr, &src_size);
+    test_error(error, "clGetProgramInfo for source size failed");
+
+    if (src_size == 0)
+    {
+        log_error("Source size is zero\n");
+        return TEST_FAIL;
+    }
+
+    std::vector<char> src(src_size, static_cast<char>(0x7f));
+
+    error = clGetProgramInfo(program_from_binary, CL_PROGRAM_SOURCE,
+                             src_size, src.data(), nullptr);
+    test_error(error, "clGetProgramInfo for source failed");
+
+    if (src.back() != '\0')
+    {
+        log_error("CL_PROGRAM_SOURCE is not null-terminated\n");
+        return TEST_FAIL;
+    }
+
+    if (src_size != 1
+        && (src_size != strlen(sample_kernel_code_single_line[0]) + 1
+            || memcmp(src.data(), sample_kernel_code_single_line[0],
+                      src_size) != 0))
+    {
+        log_error("CL_PROGRAM_SOURCE returned unexpected content\n");
+        return TEST_FAIL;
+    }
+
+    return TEST_PASS;
+}
